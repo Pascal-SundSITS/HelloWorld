b5eec739a6236cf10fcf67162a3fd9122657e8c0
Pasu <pascal.jordans@sundsits.de>
2017-08-07 15:55:32 +0200

    6

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -7,6 +7,7 @@ use Plenty\Plugin\Routing\Router;
 class HelloWorldRouteServiceProvider extends RouteServiceProvider
 {
   public function map(Router $router)
+	{
 		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
 	}
 }
